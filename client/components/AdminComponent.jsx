ffcbe0dba0280dd54bc31a04e61387673a784df0
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-06-09 16:14:34 +0100

    Admin component keeps clicks and count in its state

--- client/components/AdminComponent.jsx
+++ client/components/AdminComponent.jsx
@@ -2,15 +2,25 @@ var React = require("react");
 
 var AdminComponent = React.createClass({
 
+    getInitialState: function () {
+        return {
+            count: 0,
+            clicks: []
+        }
+    },
+
     componentDidMount: function() {
-        console.log("The admin component mounted!!")
+        this.props.ws.onmessage = this.onMessage;
     },
 
     render: function() {
         return (
             <div className="reactComponentContainer">
                 <h1 className="f-header">Admin</h1>
-                <ul id="clicks"></ul>
+                <div className="count">{this.state.count}</div>
+                <ul id="clicks">{
+                    this.state.clicks.map((click, i) => <p key={i}>{click}</p>)
+                }</ul>
                 <button className="f-headline" onClick={this.onButtonClick}>CLICK ME</button>
             </div>
         );
@@ -18,6 +28,23 @@ var AdminComponent = React.createClass({
 
     onButtonClick: function () {
         this.props.ws.send(JSON.stringify({ messageType: 'showOnAll', messageData: 'hello' }))
+    },
+
+    onMessage: function (event) {
+        var jsonEvent = JSON.parse(event.data);
+        console.log(jsonEvent);
+        if (jsonEvent.messageType === 'log') {
+            this.setState({
+                count: this.state.count + 1
+            });
+        }
+
+        if (jsonEvent.messageType === 'showOnAll') {
+            var message = 'SOMEONE CLICKED: ' + jsonEvent.messageData;
+            var clicks = this.state.clicks.slice(0);
+            clicks.push(message);
+            this.setState({clicks});
+        }
     }
 
 });
