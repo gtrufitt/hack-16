790908c4e25d8990eee7a1abe604a0dc938051ca
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-06-09 16:04:50 +0100

    Add text sans

--- client/fonts.js
+++ client/fonts.js
@@ -3,6 +3,8 @@
 
 var egyptianWeb = 'https://assets.guim.co.uk/fonts/9849697bbb3a749ce02d4b85bf0b3cd8/GuardianEgyptianWeb.woff2.json'
 var textEgyptianWeb = 'https://assets.guim.co.uk/fonts/ae8a1b17105d9a63b0da12c54a109721/GuardianTextEgyptianWeb.woff2.json'
+var sansWeb = 'https://assets.guim.co.uk/fonts/1976d273f3aeeab8863d462068fec10b/GuardianSansWeb.woff2.json'
+var textSansWeb = 'https://assets.guim.co.uk/fonts/355c7e3d0d08d7772e40e50fd5a92b7b/GuardianTextSansWeb.woff2.json'
 
 module.exports = loadFonts
 
@@ -16,7 +18,19 @@ function loadFont(fontUrl, fontName) {
 	}).then(function(font){		
 		var guFont = fontData => fontData.css;
 
-		document.querySelector('.webfont-' + fontName).innerHTML = eval(font);
+		var css = eval(font);
+
+		var head = document.head || document.getElementsByTagName('head')[0],
+		    style = document.createElement('style');
+
+		style.type = 'text/css';
+		if (style.styleSheet){
+		  style.styleSheet.cssText = css;
+		} else {
+		  style.appendChild(document.createTextNode(css));
+		}
+
+		head.appendChild(style);
 	}).catch(function(err) {
 		console.log(err);
 	});
@@ -25,4 +39,6 @@ function loadFont(fontUrl, fontName) {
 function loadFonts() {
 	loadFont(egyptianWeb, 'egyptianweb');
 	loadFont(textEgyptianWeb, 'textegyptianweb');
+	loadFont(sansWeb, 'sansweb');
+	loadFont(textSansWeb, 'textsansweb');
 }
