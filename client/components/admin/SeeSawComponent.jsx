58dffa1ace242d5ab3662287556b291046b66743
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-06-10 14:26:27 +0100

    Hook up rotation

--- client/components/admin/SeeSawComponent.jsx
+++ client/components/admin/SeeSawComponent.jsx
@@ -3,9 +3,14 @@
 var React = require("react");
 
 var SeeSawBeam = React.createClass({
+
     render : function(){
+        var {rotate} = this.props;
+
         return(
-            <div className="SeeSawBeam"></div>);
+            <div className="SeeSawBeam" style={
+                { transform: 'rotate(' + rotate + 'deg)' }
+            }></div>);
     }
 })
 
@@ -28,10 +33,20 @@ var SeeSawComponent = React.createClass({
         var p = 1/Math.max(1, brexit + remain);
         var bp = Math.round(10000 * brexit * p) / 100;
         var rp = Math.round(10000 * remain * p) / 100;
+        var rotation = 0;       
+
+        if (bp > rp) {
+            rotation = bp * -0.15;
+        }
+        if (rp > bp) {
+            rotation = rp * 0.15;
+        }
+
+        console.log(rotation);
         return (
             <div className="SeeSawComponent">
                 <h2 className="SeeSawHeading">EU Referendum Tracker</h2>
-                <SeeSawBeam/>
+                <SeeSawBeam rotate={rotation}/>
                 <div className="results">
                     <h4 className="result brexit">Brexit: {bp}%</h4>
                     <h4 className="result remain">Remain: {rp}%</h4>
