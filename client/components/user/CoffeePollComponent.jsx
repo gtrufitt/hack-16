ab3a7fb1a2c3bd6c77be786bcc96a659d7ed6f17
Lydia Shepherd <lydia.shepherd@guardian.co.uk>
2016-06-10 11:47:58 +0100

    Show how caffeinated you are in relation to the rest of the room

--- client/components/user/CoffeePollComponent.jsx
+++ client/components/user/CoffeePollComponent.jsx
@@ -7,21 +7,34 @@ var comms = require("../../utils/comms-client")();
 var CoffeePollComponent = React.createClass({
 
     getInitialState: function(){
-        return {
-            buttons: [0,1,2,3,4,5,6,7]
+        var numbers = [0,1,2,3,4,5,6,7];
+        var state = {numbers, width:500};
+        for(var i in numbers){
+            state['had' + i] = 0;
         }
+        return state
     },
 
     componentDidMount: function() {
         console.log("The coffee poll component mounted!!")
+        this.props.ws.onmessage = this.onMessage;
     },
 
     render: function() {
+        var that = this;
+        var totalVotes = this.state.numbers.map(i => this.state["had" + i]);
+        var lessCaffeinated = 0;
+        var total = totalVotes.reduce(function(previousValue, currentValue, currentIndex) {
+            if (currentIndex === that.state.numberOfCoffees) lessCaffeinated = previousValue;
+            return previousValue + currentValue;
+        });
+        var p = 100 * lessCaffeinated/(Math.max(total - 1, 1));
+
         return (
             <div className="coffeePollComponent">
                 <h2 className="f-header">Question 1</h2>
                 <h3 className="f-header">How many coffees have you had today?</h3>
-                {!this.state.numberOfCoffees && this.state.buttons.map((_, i)=> (
+                {!this.state.numberOfCoffees && this.state.numbers.map((_, i)=> (
                     <button
                         key={i}
                         className="admin-btn f-textSans"
@@ -29,9 +42,19 @@ var CoffeePollComponent = React.createClass({
                         i || "tea"
                     }</button>
                 ))}
-                {!!this.state.numberOfCoffees && <p>
-                    Thanks for voting! Watch for the results on the big screen...
-                </p>}
+                {!!this.state.numberOfCoffees &&
+                <div>
+                    <p>
+                        Thanks for voting!
+                    </p>
+                    <p>
+                        You are more caffeinated than {p}% of today's hackers.
+                    </p>
+                    <p>
+                        Watch for the results on the big screen...
+                    </p>
+                </div>
+                }
             </div>
         );
     },
@@ -44,6 +67,17 @@ var CoffeePollComponent = React.createClass({
                 numberOfCoffees: i
             }
         });
+    },
+
+    onMessage: function(event) {
+        var jsonEvent = JSON.parse(event.data);
+
+        if (jsonEvent.messageType === 'coffeeVote') {
+            var number = jsonEvent.messageData.numberOfCoffees;
+            var newState = {};
+            newState['had'+ number] = this.state['had'+ number] + 1;
+            this.setState(newState);
+        }
     }
 
 });
